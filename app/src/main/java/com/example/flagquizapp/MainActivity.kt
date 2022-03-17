9c4ed38d22591b707a3b0d3633cb8c40ddcc1f67
Vivian Li <vivian2938@gmail.com>
2022-03-17 02:05:13 -0400

    animation between activities

--- app/src/main/java/com/example/flagquizapp/MainActivity.kt
+++ app/src/main/java/com/example/flagquizapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.flagquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), AppContract.MainView {
+open class MainActivity : AppCompatActivity(), AppContract.MainView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +20,30 @@ class MainActivity : AppCompatActivity(), AppContract.MainView {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.top_app_bar, menu);
         return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar items
+        return when (item.getItemId()) {
+            R.id.quizItem -> {
+                //composeMessage();
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.right_in, R.anim.left_out)
+                Log.d("menu", "quiz")
+                true
+            }
+            R.id.settingsItem -> {
+                //showProfileView()
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.left_in, R.anim.right_out)
+                Log.d("menu", "settings")
+                true
+            }
+            else ->
+                super.onOptionsItemSelected(item)
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/flagquizapp/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.flagquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class SettingsActivity : MainActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+
+
+}
